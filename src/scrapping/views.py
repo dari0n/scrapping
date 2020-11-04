b195ab6c0512a3b03d28df5d4fba50d589e7c611
dari0n <79887423414@yandex.ru>
2020-11-04 11:09:09 +0300

    Base views and view functions

--- src/scrapping/views.py
+++ src/scrapping/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from .models import Vacancy
 # Create your views here.
+
+
+def home_view(request):
+    objects_list = Vacancy.objects.all()
+    return render(request, 'scrapping/home.jinja2', {'objects_list' : objects_list})
